f26c52ff7942f77dae6e99671c7845443e2dfd0c
Hadi Rezaei <hadirezaei.etc.esfahan@gmail.com>
2024-10-27 17:01:42 +0330

    add: serve-config-file

--- main.go
+++ main.go
@@ -2,17 +2,23 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 	"os"
 
-	echo "github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4"
 )
 
 func main() {
 	router := echo.New()
 
 	router.GET("/health-check", healthCheck)
+	router.GET("/serve-config-file", serveConfigFile)
+	router.GET("/show-me-password", showMePassword)
+
+	staticFilePath := getEnv("ARMANDO_STATIC_PATH", "./static-files")
+	router.Static("/statics", staticFilePath)
 
 	port := getEnv("ARMANDO_HTTP_PORT", "8080")
 
@@ -27,9 +33,35 @@ func healthCheck(c echo.Context) error {
 	})
 }
 
+func showMePassword(c echo.Context) error {
+	key := getEnv("ARMANDO_SECRET_KEY", "no password")
+	return c.JSON(http.StatusOK, echo.Map{
+		"secret-key": key,
+	})
+}
+
+func serveConfigFile(c echo.Context) error {
+	filePath := getEnv("ARMANDO_CONFIG_FILE_PATH", "config.yml")
+
+	f, err := os.Open(filePath)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
+	}
+
+	content, rErr := io.ReadAll(f)
+	if rErr != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError, rErr.Error())
+	}
+
+	return c.JSON(http.StatusOK, echo.Map{
+		"data": string(content),
+	})
+}
+
 func getEnv(key, fallback string) string {
 	if v := os.Getenv(key); v != "" {
 		return v
 	}
+
 	return fallback
 }
